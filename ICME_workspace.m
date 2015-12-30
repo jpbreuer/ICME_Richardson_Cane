41b3a5a005a344b87f981ce7c3d6ae631d9c7f7c
jpbreuer <jeanpaul.breuer@gmail.com>
2015-12-30 16:37:32 +0100

    optimized code, now running the first time creates mat file, if mat file already exists then it uses that instead of pulling.

--- ICME_workspace.m
+++ ICME_workspace.m
@@ -3,5 +3,8 @@
 clear all
 close all
 
-
-[jdssc_rich, jds_icme, jde_icme, jds_mc, jde_mc, bde, bif, quality, V_ICME, vpeak, B, MC, Hut, dstpeak, dstpeak_info, v_transit, jd_lascotime, lasco_datagap, Halo] = get_ICME_tabledata();
+tic
+[jdssc_rich, jds_icme, jde_icme, jds_mc, jde_mc, bde, bif, quality,...
+    V_ICME, vpeak, B, MC, Hut, dstpeak, dstpeak_info, v_transit,...
+    jd_lascotime, lasco_datagap, Halo] = get_ICME_tabledata();
+toc
